ddc1daebf0ad76053d97dbb3e451c553e57b8480
Stephen Murray <stephen.patrick.murray@gmail.com>
2025-05-26 12:04:10 -0400

    feat: add error enum

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("HTTP error: {0}")]
+    Http(#[from] reqwest::Error),
+
+    #[error("API error: {0}")]
+    Api(String),
+
+    #[error("Serialization error: {0}")]
+    Serialization(#[from] serde_json::Error),
+
+    #[error("Missing API key")]
+    MissingApiKey,
+
+    #[error("Unexpected response format")]
+    UnexpectedResponse,
+}
